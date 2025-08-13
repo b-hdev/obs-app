87758fe8d2c184313dbdb22a0080872f83524d36
b-hdev <bruno.bh969@gmail.com>
2025-08-13 01:29:03 -0300

    fix: minio error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/metrics')
+  getMetrics(): string {
+    return this.appService.getMetrics();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { metrics } from './tracer';
+import { log } from './infra/logger';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    const meter = metrics.getMeter('obs-app');
+    const successMeter = meter.createCounter('success_requests');
+    successMeter.add(1);
+    log.info('Cheguei aqui!');
     return 'Hello World!';
+  }
+
+    getMetrics(): string {
+    const meter = metrics.getMeter('obs-app');
+    const errorMeter = meter.createCounter('error_requests');
+    errorMeter.add(1);
+    const histogram = meter.createHistogram('request_duration');
+    histogram.record(1000);
+    return 'Metrics World!';
   }
 }
